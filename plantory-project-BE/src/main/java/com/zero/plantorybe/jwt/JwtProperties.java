f4561d09241aa1d87e0b8b3f5029ffdc5c133466
KBH <galaxysgoo418@gmail.com>
2025-12-14 13:49:26 +0900

    refactor: project name

--- plantory-project-BE/src/main/java/com/zero/plantorybe/jwt/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.zero.plantorybe.jwt;
-
-public interface JwtProperties {
-    String SECRET = "plantory";
-    int EXPIRATION_TIME = 1000*60*60; //1시간
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
-}
